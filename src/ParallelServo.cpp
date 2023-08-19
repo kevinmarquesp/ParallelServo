8d718e09a548118de2bf605e40739096771a3cbc
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-08-18 22:25:47 -0300

    impl: created the logic for the move() method

--- src/ParallelServo.cpp
+++ src/ParallelServo.cpp
@@ -28,3 +28,42 @@ void ParallelServo::begin(u8 pin, i16 min=0, i16 max=180)
   this->attach(_pin); // methods from the Servo.h
   this->write(_min);  // move the motor to the min value at the beggining
 }
+
+// todo: write the update move and the update postion methods
+
+ParallelServo* ParallelServo::move(bool condition, u8 deg, u8 speed)
+{
+  // this line just ajust the deg value to the correct range (between _min and _max)
+  deg = deg < _min ?
+    _min : deg > _max ?
+      _max : deg;
+
+  if (!condition || _isLocked)
+    return this;
+  else
+    _isDone = false; // reset done value when the movement starts
+
+  // it's unecessary to do any math when the speed is 0, just go ahead and finish the movemt
+  if (speed < 1)
+  {
+    this->write(deg);
+    _stopAndMarkMovementAsDone();
+  }
+
+  if (!_isMoving && !_isDone)
+  {
+    _mpc = millis();
+    _isMoving = true;
+  }
+  else if (_isMoving && !_isDone)
+  {
+    _updateCurrentPosition(deg, speed);
+  }
+
+  return this;
+}
+
+ParallelServo* ParallelServo::move(u8 deg, u8 speed)
+{
+  return this->move(_index == 0, deg, speed);
+}
