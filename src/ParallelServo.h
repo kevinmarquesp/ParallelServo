bbf228e12662a7d8ed8a4c27c78e41ca74407455
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-08-19 14:58:01 -0300

    docs: added a todo to remove that this->read() thing

--- src/ParallelServo.h
+++ src/ParallelServo.h
@@ -30,6 +30,7 @@
 // todo: mention on the description how the movements is counted
 // todo: edit the docs to recoment to reset using reset() instead of setIndex(0)
 // todo: edit the being() jsdoc to be consistency with the types
+// todo: remove the this->read() and replace it to the _pasPos
 
 /*!
  * A little side note: when a movement is completed, the index of this object
